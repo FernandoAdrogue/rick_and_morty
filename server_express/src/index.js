491a72d9efde8cb2443c8cd707c912fb1dbf947a
FernandoAdrogue <chinoadrogue@gmail.com>
2023-06-10 11:03:04 -0300

    homework-07 integration test complete

--- server_express/src/index.js
+++ server_express/src/index.js
@@ -1,28 +1,29 @@
-const express = require("express")
-const morgan = require("morgan")
-const server = express()
-const router = require("./routes/index")
+//const express = require("express")
+const server = require("./app")
 const PORT = 3001
 
-server.use(morgan("dev"))
+//const morgan = require("morgan")
+//const router = require("./routes/index")
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
+// server.use(morgan("dev"))
 
-server.use(express.json())
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header(
+//      'Access-Control-Allow-Headers',
+//      'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header(
+//      'Access-Control-Allow-Methods',
+//      'GET, POST, OPTIONS, PUT, DELETE'
+//   );
+//   next();
+// });
 
-server.use("/rickandmorty", router)
+// server.use(express.json())
+
+// server.use("/rickandmorty", router)
 
 server.listen(PORT,()=>{
   console.log(`Server raised in http://localhost:${PORT}`);
